8d67fec67ff1cb9a11669106c1f3def0b8b41e33
Luis Fernando do Rosario Cintra <82674962+fernandocintra2871@users.noreply.github.com>
2024-07-24 18:19:14 -0300

    Comentarios e README

--- app.py
+++ app.py
@@ -4,28 +4,23 @@ import json
 import os
 from time import sleep
 
-"""
-$env:host="127.0.0.1"; $env:port="12001"; $env:clock2_host="127.0.0.1"; $env:clock2_port="12002"; $env:clock3_host="127.0.0.1"; $env:clock3_port="12003"; ; python3 app.py 
-$env:port="12001"; $env:clock2_port="12002"; $env:clock3_port="12003"; python3 app.py
-$env:port="12002"; $env:clock2_port="12001"; $env:clock3_port="12003"; python3 app.py 
-$env:port="12003"; $env:clock2_port="12002"; $env:clock3_port="12001"; python3 app.py 
+# Obtenção dos valores das variáveis de ambiente
+host = os.environ.get("host")
+port = 12001
 
-"""
+clock2_host = os.environ.get("clock2_host")
+clock2_port = 12001
 
-host = "127.0.0.1"
-port = int(os.environ.get("port"))
+clock3_host = os.environ.get("clock3_host")
+clock3_port = 12001
 
-clock2_host = "127.0.0.1"
-clock2_port = int(os.environ.get("clock2_port"))
-
-clock3_host ="127.0.0.1"
-clock3_port = int(os.environ.get("clock3_port"))
-
+# Variáveis globais
 time = 0
 drift = 1
 clocks = {}
 entry_detection = False
 
+# Função para lidar com clientes conectados ao servidor
 def handle_client(client_socket, client_address):
     global time
     #print(f'Conexão estabelecida com {client_address}')
@@ -36,10 +31,11 @@ def handle_client(client_socket, client_address):
         message = json.loads(data.decode())
         #print(f'Recebido de {client_address}: {message}')
 
+        # Responde com o tempo atual se a mensagem recebida for "TIME"
         if message == "TIME":
-            response_message = {"code": 1, "time":time}
+            response_message = {"code": 1, "time": time}
         else:
-            response_message = {"code": 0, "error_message":"Invalid Command"}
+            response_message = {"code": 0, "error_message": "Invalid Command"}
         
         response = json.dumps(response_message)
         client_socket.sendall(response.encode())
@@ -47,6 +43,7 @@ def handle_client(client_socket, client_address):
     client_socket.close()
     #print(f'Conexão com {client_address} encerrada')
 
+# Função para iniciar o servidor
 def start_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
@@ -58,6 +55,7 @@ def start_server(host, port):
         client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
         client_handler.start()
 
+# Função para iniciar o cliente e se conectar a um servidor de relógio
 def start_client(clock_id, host, port):
     global clocks
     connected = False
@@ -89,6 +87,7 @@ def start_client(clock_id, host, port):
                 print(message["error_message"])
             sleep(0.5)
 
+# Função para simular a deriva do tempo
 def drift_time():
     global time
     global drift
@@ -102,27 +101,31 @@ def drift_time():
             time = max(times) + 1
         else:
             time = time + 1
-        if not(entry_detection):
+        if not entry_detection:
             clear_console()
+            print("Tempo dos Relogios Conectados")
             print(times_list)
+            print("Relogio")
             print(time)
 
-# Limpa o console
+# Função para limpar o console
 def clear_console():
-    # Verifica o sistema operacional e executa o comando apropriado
-    if os.name == "posix": # Linux e macOS
+    if os.name == "posix":
         os.system("clear")
-    elif os.name == "nt": # Windows
+    elif os.name == "nt":
         os.system("cls")
     else:
         print("Não foi possível limpar a tela para este sistema.")
 
+# Inicializa a thread para simular a deriva do tempo
 drift_time_thread = threading.Thread(target=drift_time)
 drift_time_thread.start()
 
+# Inicializa a thread do servidor
 server_thread = threading.Thread(target=start_server, args=(host, port), daemon=True)
 server_thread.start()
 
+# Inicializa as threads dos clientes (relógios)
 clock1_thread = threading.Thread(target=start_client, args=("1", host, port), daemon=True)
 clock1_thread.start()
 
@@ -132,9 +135,10 @@ clock2_thread.start()
 clock3_thread = threading.Thread(target=start_client, args=("3", clock3_host, clock3_port), daemon=True)
 clock3_thread.start()
 
+# Loop principal para ajustar a deriva manualmente
 while True:
     input()
     entry_detection = True
     clear_console()
-    drift = float(input("New  drift: "))
+    drift = float(input("New drift: "))
     entry_detection = False
